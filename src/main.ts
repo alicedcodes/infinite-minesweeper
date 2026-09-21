68b741ff36d8823de464f7feb5752052db6b00c2
Alice Driscoll <alicedcodes@gmail.com>
2026-09-21 18:20:53 +1000

    feat(game): add animation frame render loop

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,35 @@ if (!canvas) throw new Error("Could not get #app element.");
 const ctx = canvas.getContext("2d", { alpha: false })!;
 if (!ctx) throw new Error("Browser does not support canvas.");
 
+let canvasWidth = 0;
+let canvasHeight = 0;
+
+let dirty = true;
+
+function draw(): void {
+  ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+}
+
+function tick(): void {
+  if (dirty) {
+    draw();
+    dirty = false;
+  }
+
+  requestAnimationFrame(tick);
+}
+
 const observer = new ResizeObserver((entries) => {
   const entry = entries[0]!;
   const { width, height } = entry.contentRect;
 
   canvas.width = width;
   canvas.height = height;
+  canvasWidth = width;
+  canvasHeight = height;
+
+  dirty = true;
 });
 observer.observe(canvas);
+
+requestAnimationFrame(tick);
