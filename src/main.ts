05d6a68d496a93f98166a72898ece54878df09a4
Alice Driscoll <alicedcodes@gmail.com>
2026-09-21 18:04:39 +1000

    feat(canvas): initialise 2d rendering context

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import "./style.css";
 
 const canvas = document.querySelector<HTMLCanvasElement>("#app")!;
 if (!canvas) throw new Error("Could not get #app element.");
+const ctx = canvas.getContext("2d", { alpha: false })!;
+if (!ctx) throw new Error("Browser does not support canvas.");
 
 const observer = new ResizeObserver((entries) => {
   const entry = entries[0]!;
